e26edcbfad5782d058379bfdb3ceb921e019be70
signsandwonders <73362951+signsandwonders@users.noreply.github.com>
2021-03-07 09:17:19 -0700

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,10 @@
+input.onButtonPressed(Button.A, function () {
+    music.playMelody("C5 B A G F E D C ", 120)
+})
+input.onButtonPressed(Button.B, function () {
+    music.playMelody("F E B E A E B E ", 120)
+})
+basic.showString("daniel")
+basic.forever(function () {
+	
+})
